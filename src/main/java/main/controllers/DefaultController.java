8d837c3c7fdab5757e8b7564deee3aab1693728d
Zhadov Dmitriy <64922630+Dalton008@users.noreply.github.com>
2021-09-21 17:20:44 +0300

    add login and registration

--- src/main/java/main/controllers/DefaultController.java
+++ src/main/java/main/controllers/DefaultController.java
@@ -5,6 +5,7 @@ import main.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -16,7 +17,20 @@ public class DefaultController
     TaskRepository taskRepository;
 
     @RequestMapping("/")
-    public String index(Model model)
+    public String home(Model model)
+    {
+        Iterable<Task> taskIterable = taskRepository.findAll();
+        ArrayList<Task> tasks = new ArrayList<>();
+        for (Task task : taskIterable)
+        {
+            tasks.add(task);
+        }
+        model.addAttribute("title", "ToDoList Главная страница");
+        return "home";
+    }
+
+    @RequestMapping("/tasks")
+    public String tasks(Model model)
     {
         Iterable<Task> taskIterable = taskRepository.findAll();
         ArrayList<Task> tasks = new ArrayList<>();
@@ -25,8 +39,8 @@ public class DefaultController
             tasks.add(task);
         }
         model.addAttribute("tasks", tasks);
-        model.addAttribute("title", "Главная страница");
+        model.addAttribute("title", "Tasks");
 
-        return "index";
+        return "tasks";
     }
 }
---
+++ src/main/java/main/model/User.java
@@ -0,0 +1,29 @@
+package main.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String username;
+
+    private String password;
+
+    private boolean active;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+}
